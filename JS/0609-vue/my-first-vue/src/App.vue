3c11bbd9df49b7b09f7b1305cbdb16ccb7392f59
Bruce <love890313@gmail.com>
2026-07-20 18:08:34 +0800

    0720

---
+++ JS/0609-vue/my-first-vue/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import {ref} from 'vue' 
+const count=ref(0)
+</script>
+
+<template>
+  <div>
+    <button @click="count++">點了 {{ count }} 次</button>
+  </div>
+</template>
+
+<style scoped></style>
